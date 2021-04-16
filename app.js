c0a22f16a1e82cab6b0cb29938d871395c6fc0e2
aadhar54 <aadhar54@gmail.com>
2021-04-16 20:12:23 +0530

    api logic done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let btn = document.getElementById("#btn");
+let btn = document.getElementById('btn');
+let inputTextArea = document.getElementById('input-txtarea');
+let outputDiv = document.getElementById('output-div');
 
 function playAudio(){
     let audio = new Audio('./sounds/bell.mp3');
@@ -6,5 +8,10 @@ function playAudio(){
 }
 
 btn.addEventListener('click',()=>{
+    let input = inputTextArea.value;
+    fetch('https://api.funtranslations.com/translate/morse.json?text='+input)
+    .then(resonse=>resonse.json)
+    .then((json)=>{
+        outputDiv.innerText = json.contents.translated;
+    })
 })
-
