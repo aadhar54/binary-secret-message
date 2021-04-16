010a07bf661104869a9a1d3f10f5dcceef837522
aadhar54 <aadhar54@gmail.com>
2021-04-16 15:51:39 +0530

    front-end ready

---
+++ app.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("#btn");
+
+function playAudio(){
+    let audio = new Audio('./sounds/bell.mp3');
+    audio.play();
+}
+
+btn.addEventListener('click',()=>{
+})
+
